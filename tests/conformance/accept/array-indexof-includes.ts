06a7e54e9b60a046a4c1e62554aca8987046a3e5
Jesse Alama <hello@jessealama.net>
2026-06-17 08:51:29 +0200

    Add accept examples for Array join/indexOf/includes

---
+++ tests/conformance/accept/array-indexof-includes.ts
@@ -0,0 +1,11 @@
+const xs: number[] = [3, 1, 2];
+console.log(xs.indexOf(2));
+console.log(xs.indexOf(9));
+console.log(xs.includes(3));
+console.log(xs.includes(9));
+
+const ss: string[] = ['a', 'b'];
+console.log(ss.indexOf('b'));
+console.log(ss.indexOf('z'));
+console.log(ss.includes('a'));
+console.log(ss.includes('z'));
---
+++ tests/conformance/accept/array-join.ts
@@ -0,0 +1,11 @@
+const xs: number[] = [3, 1, 2];
+console.log(xs.join(','));
+console.log(xs.join());
+console.log(xs.join(' - '));
+
+const ss: string[] = ['a', 'b', 'c'];
+console.log(ss.join(''));
+console.log(ss.join(','));
+
+const empty: number[] = [];
+console.log(empty.join(','));
